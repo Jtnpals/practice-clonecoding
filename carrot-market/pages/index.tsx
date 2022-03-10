823e7c6c6b166f45eb5cf367e87994986defa1e9
Jtnpals <46595593+Jtnpals@users.noreply.github.com>
2022-03-10 14:02:58 +0900

    First component using the tailwindcss

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-slate-400 py-10 px-5 grid gap-10">
+      <div className="bg-white p-6 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Gray Chair</span>
+          <span>$19</span>
+        </div>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Gray Chair</span>
+          <span>$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span>$10</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
+  );
 };
 
 export default Home;
