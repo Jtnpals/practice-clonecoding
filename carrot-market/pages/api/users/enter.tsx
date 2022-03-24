3329f0b76314dc16b3d2b4737a51d8b35b6920bb
Jtnpals <46595593+Jtnpals@users.noreply.github.com>
2022-03-24 17:58:05 +0900

    Update handler.tsx

--- carrot-market/pages/api/users/enter.tsx
+++ carrot-market/pages/api/users/enter.tsx
@@ -1,9 +1,37 @@
-import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.json({ ok: true });
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) {
+    res.status(400).json({ ok: false });
+  }
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  console.log(user);
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
