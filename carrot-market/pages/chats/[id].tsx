35e15e59f18dec5cc573c91f76c7b14d2cb42cd9
Jtnpals <46595593+Jtnpals@users.noreply.github.com>
2022-05-16 14:37:10 +0900

    wip

--- carrot-market/pages/chats/[id].tsx
+++ carrot-market/pages/chats/[id].tsx
@@ -5,18 +5,18 @@ import Message from "@components/message";
 const ChatDetail: NextPage = () => {
   return (
     <Layout canGoBack title="Steve">
-      <div className="py-10 pb-16 px-4 space-y-4">
+      <div className="px-4 py-10 pb-16 space-y-4">
         <Message message="Hi how much are you selling them for?" />
         <Message message="I want ￦20,000" reversed />
         <Message message="미쳤어" />
-        <form className="fixed py-2 bg-white  bottom-0 inset-x-0">
-          <div className="flex relative max-w-md items-center  w-full mx-auto">
+        <form className="fixed inset-x-0 bottom-0 py-2 bg-white">
+          <div className="relative flex items-center w-full max-w-md mx-auto">
             <input
               type="text"
-              className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+              className="w-full pr-12 border-gray-300 rounded-full shadow-sm focus:ring-orange-500 focus:outline-none focus:border-orange-500"
             />
             <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
+              <button className="flex items-center px-3 text-sm text-white bg-orange-500 rounded-full focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 hover:bg-orange-600">
                 &rarr;
               </button>
             </div>
