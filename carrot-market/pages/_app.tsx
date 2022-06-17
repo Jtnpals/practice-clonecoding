9567d5b23af75c5c80075c15b26ad1ee475f63b7
Jtnpals <46595593+Jtnpals@users.noreply.github.com>
2022-06-17 15:32:55 +0900

    Initial project

--- carrot-market/pages/_app.tsx
+++ carrot-market/pages/_app.tsx
@@ -1,20 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { SWRConfig } from "swr";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SWRConfig
-      value={{
-        refreshInterval: 3000,
-        fetcher: (url: string) => fetch(url).then((r) => r.json()),
-      }}
-    >
-      <div className="w-full max-w-xl mx-auto">
-        <Component {...pageProps} />
-      </div>
-    </SWRConfig>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
